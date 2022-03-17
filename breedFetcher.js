03fb66a332828c43b76ff1c2c1edbb54e7b028c7
mariohamameh <mariohamameh97@gmail.com>
2022-03-17 19:06:13 +0000

    returns breed correctly

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+
+const fetchBreedDescription = (breedName, callback) => {
+
+    request(('https://api.thecatapi.com/v1/breeds/search?q=' + breedName), (error, response, body) => {
+  
+      if (error) {
+        return callback(error);
+      } else {
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          return callback('Not valid');
+        } else {
+          return callback(null, data[0].description);
+        }
+      }
+  
+    });
+  
+  };
+  
+  module.exports = { fetchBreedDescription };
